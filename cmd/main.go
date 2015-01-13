ef1d64598a5d90c83aba6927d4e848d8d94182da
Richard Bucker <Richard_Bucker@ultimatesoftware.com>
2015-01-13 16:13:30 -0500

    prepend the GO if it's a go sub-command; capture the command output and wait until it's complete

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
 	"path"
+	"strings"
 )
 
-var help bool
+var (
+	help     bool
+	commands = ",build,clean,env,fix,fmt,generate,get,install,list,run,test,tool,version,vet,"
+)
 
 // Usage function to helping the command line
 var Usage = func(msg string) {
@@ -20,11 +26,36 @@ var Usage = func(msg string) {
 }
 
 func main() {
+	var err error
 	flag.Parse()
 	if help {
 		Usage("")
 	}
 	// golo command [arguments]
+	cmdargs := strings.Join(flag.Args(), " ")
+	if strings.Index(commands, ","+flag.Arg(0)+",") >= 0 {
+		cmdargs = "go " + cmdargs
+	}
+
+	cmd := exec.Command("/bin/bash", "-c", cmdargs)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Printf("Error creating pipe %v\n", err)
+		return
+	}
+	if err = cmd.Start(); err != nil {
+		fmt.Printf("Error starting the command; %v\n", err)
+		return
+	}
+	r := bufio.NewReader(stdout)
+	for err == nil {
+		if line, _, err := r.ReadLine(); err == nil {
+			fmt.Println(string(line))
+			continue
+		}
+		break
+	}
+	cmd.Wait()
 }
 
 func init() {
