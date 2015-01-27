946d38a113c374136d35e6aa1ae480f2ec96cfd1
Mathias Gumz <mg@2hoch5.com>
2015-01-27 09:39:56 +0100

    fix mk_release.sh helper

--- mk_release.sh
+++ mk_release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+D=`date -u +'%Y-%m-%y %H:%M:%S'`
+G=`git rev-parse --short HEAD`
+
 go build -v \
-    -ldflags "-X main._VERSION_MINOR $1"\
-    -ldflags "-X main._DATE `date -u +%Y-%m-%y.%H:%M:%S`" \
-    -ldflags "-X main._COMMIT_ID `git rev-parse --short HEAD`"
+    -ldflags "-X main._VERSION_MINOR \"$1\" -X main._DATE \"$D\" -X main._COMMIT_ID \"$G\""
